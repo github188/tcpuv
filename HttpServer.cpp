c261c5f7ded3660817b8dcf30b913d992bae8e8a
wangzhen625 <670615918@qq.com>
2016-07-17 17:36:39 +0800

    add httpserver

---
+++ HttpServer.cpp
@@ -0,0 +1,412 @@
+﻿#include "HttpServer.h"
+#include <stdlib.h>
+#include <assert.h> 
+#include <string.h>
+#include "yt/log/log.h"
+using namespace std;
+
+HttpRequest* allocHttpRequestCtx(void* parentserver)
+{
+    HttpRequest* ctx = (HttpRequest*)malloc(sizeof(HttpRequest));
+    ctx->recvBuf.base = (char*)malloc(BUFFER_SIZE);
+    ctx->recvBuf.len = BUFFER_SIZE;
+    ctx->parent_server = parentserver;	
+    return ctx;
+}
+
+void freeHttpRequestCtx(HttpRequest* ctx)
+{
+    free(ctx->recvBuf.base);
+    free(ctx);
+} 
+
+WriteReq_t * allocWriteParam(void)
+{
+	WriteReq_t * writeReq = (WriteReq_t*)malloc(sizeof(WriteReq_t));
+	writeReq->buf.base = (char*)malloc(BUFFER_SIZE);
+	return writeReq;
+}
+
+void freeWriteParam(WriteReq_t* param)
+{
+	free(param->buf.base);
+    free(param);
+}
+
+HttpServer::HttpServer(int maxClientNum, int maxPackageSize):maxPackageSize_(maxPackageSize),maxClientNum_(maxClientNum)
+{
+	
+}
+
+HttpServer::~HttpServer()
+{
+    uv_mutex_destroy(&mutexWrite_);
+    //uv_mutex_destroy(&mutexContext_);
+    for (std::map<int, HttpRequest *>::iterator it = requestMap_.begin(); it != requestMap_.end(); ++it) {         
+        freeHttpRequestCtx(it->second);
+    }
+    requestMap_.clear();
+    for (std::list<WriteReq_t*>::iterator it = writeReqList_.begin(); it != writeReqList_.end(); ++it) {
+        freeWriteParam(*it);
+    }
+    writeReqList_.clear();
+}
+
+void HttpServer::join()
+{
+    uv_thread_join(&runThreadHandle_);
+}
+
+void HttpServer::close()
+{
+    uv_stop(&loop_);
+    uv_walk(&loop_, closeWalkCallback, this);  
+}
+
+void HttpServer::closeWalkCallback(uv_handle_t* handle, void* arg)  //回调多次
+{
+    //TCPClient* pclient = (TCPClient*)arg;
+    if (uv_is_active(handle)) {      
+        uv_close(handle, NULL);
+        if (handle->type == UV_ASYNC) {         
+            uv_async_send((uv_async_t*)handle);
+        }    
+    }
+}
+
+bool HttpServer::init()
+{
+	int ret = uv_loop_init(&loop_);
+    if (ret) {
+        AC_ERROR("uv_loop_init error,%s", getUVError(ret).c_str());
+        return false;
+    }
+
+    ret = uv_mutex_init(&mutexWrite_);
+    if (ret) {
+         AC_ERROR("uv_mutex_init error,%s", getUVError(ret).c_str());
+         return false;
+    }
+
+	ret = uv_async_init(&loop_, &asyncHandle_, asyncCallback);
+	if (ret) {
+		 AC_ERROR("uv_async_init error,%s", getUVError(ret).c_str());
+		return false;
+	}
+	asyncHandle_.data = this;
+   
+	ret = uv_tcp_init(&loop_, &serverTcpHandle_);
+	if (ret) {
+		AC_ERROR("uv_tcp_init error,%s", getUVError(ret).c_str());
+		return false;
+	}
+    serverTcpHandle_.data = this;
+    
+
+    ret = uv_tcp_nodelay(&serverTcpHandle_,  1);
+    if (ret) {
+        AC_ERROR("uv_tcp_nodelay error,%s", getUVError(ret).c_str());
+        return false;
+    }
+
+    parserSettings_.on_url = httpUrlCallback;
+    parserSettings_.on_body = httpBodyCallback;
+    parserSettings_.on_header_field = httpHeaderFeildCallback;
+    parserSettings_.on_header_value = httpHeaderValueCallback;
+    parserSettings_.on_headers_complete = httpHeadersCompleteCallback;
+    parserSettings_.on_message_begin = httpMessageBeginCallback;
+    parserSettings_.on_message_complete = httpMessageCompleteCallBack;
+
+
+	return true;
+}
+
+bool HttpServer::start(const char* ip, int port)
+{
+	init();
+	serverIp_ = ip;
+	serverPort_ = port;
+
+	struct sockaddr_in bindAddr;
+	uv_ip4_addr(ip, port, &bindAddr);
+
+    uv_tcp_bind(&serverTcpHandle_, (struct sockaddr*)&bindAddr, 0);
+    
+	int r = uv_listen((uv_stream_t*) &serverTcpHandle_, 128, onAcceptConnectionCallback);
+    if(r) {
+        if (r == UV_EADDRINUSE) {
+            AC_ERROR("the port already be used!");
+        } else {
+            AC_ERROR("listen error, %s",  getUVError(r).c_str());
+        }
+        return false;
+    }
+    
+	r = uv_thread_create(&runThreadHandle_, loopRunThread, this);  
+    if (r) {
+      	AC_ERROR("uv_thread_create error,%s", getUVError(r).c_str());
+        return false;
+    }
+	return true;
+}
+
+void HttpServer::loopRunThread(void* arg)
+{
+	HttpServer* pclient = (HttpServer*)arg;
+    pclient->run();
+}
+
+void HttpServer::onAcceptConnectionCallback(uv_stream_t* server, int status)
+{   
+	HttpServer* parent = (HttpServer*)server->data;
+    if (parent == NULL) {
+        AC_ERROR("Lost HttpServer handle");
+        return;
+    }
+    //assert(parent);
+
+	if (status) {
+        AC_ERROR("client Connect failed,%s", parent->getUVError(status).c_str());
+        return;
+    }
+    
+    HttpRequest* request = allocHttpRequestCtx(parent);
+    int r = uv_tcp_init(&parent->loop_, &request->tcpHandle);
+    if (r) {
+        AC_ERROR("client tcp_init error,%s", parent->getUVError(r).c_str());
+        return;
+    }
+    request->tcpHandle.data = request;
+    request->parser.data = request;
+
+    r = uv_accept(server, (uv_stream_t*) &request->tcpHandle);
+    if (r) {
+        AC_ERROR("error accepting connection ");
+        uv_close((uv_handle_t*) &request->tcpHandle, NULL);
+        return;
+    } else {
+        request->clientid = parent->getAvailableClientID();
+        AC_INFO("client %d connected!", request->clientid);
+        http_parser_init(&request->parser, HTTP_REQUEST);
+    }
+    
+    parent->requestMap_.insert(make_pair(request->clientid, request));
+
+    r = uv_read_start((uv_stream_t *)&request->tcpHandle, allocBufForRecvCallback, onReadCallback);
+	if (r) {
+        uv_close((uv_handle_t*)&request->tcpHandle, onClientCloseCallback);
+		AC_ERROR("uv_read_start error:%d", parent->getUVError(r).c_str());
+        return;
+	}
+}
+
+void HttpServer::asyncCallback(uv_async_t* handle)
+{
+    HttpServer* self = (HttpServer*)handle->data;
+    self->sendToClient();                      //回调后发送数据
+}
+
+int HttpServer::send(int client_id, char* data, int len)
+{
+    if (!data || len < 0) {
+        return -1;
+    }
+    uv_async_send(&asyncHandle_);              //产生回调, 查看之前的数据发送完没有，没完就发送                       
+    WriteReq_t *writereq = allocWriteParam();
+    writereq->req.data = this;
+    memcpy(writereq->buf.base, data, len);
+    writereq->buf.len = len;
+    writereq->clientid = client_id;
+    writeReqList_.push_back(writereq);
+    uv_async_send(&asyncHandle_);
+    return 0;
+}
+
+int HttpServer::sendToClient()
+{
+    WriteReq_t* writereq = NULL;
+    while (!writeReqList_.empty()) {
+        uv_mutex_lock(&mutexWrite_);
+        writereq = writeReqList_.front();
+        writeReqList_.pop_front();
+        uv_mutex_unlock(&mutexWrite_);
+
+        map<int, HttpRequest *>::iterator iter = requestMap_.find(writereq->clientid);
+        if (iter == requestMap_.end()) {
+            continue;
+        }   
+
+        uv_write((uv_write_t*)&writereq->req, (uv_stream_t*)&iter->second->tcpHandle, &writereq->buf, 1, onWriteCallback);
+    }
+    return 0;
+}
+
+void HttpServer::onWriteCallback(uv_write_t* req, int status)
+{
+    HttpServer* self = (HttpServer*)req->data;
+    WriteReq_t *writereq = (WriteReq_t*)req;
+    if (status < 0) {
+        AC_ERROR("send response error, %d ", self->getUVError(status).c_str());
+        return;
+    } 
+    uv_close((uv_handle_t*) req->handle, NULL);
+}
+
+
+void HttpServer::onClientCloseCallback(uv_handle_t* handle)
+{
+    HttpRequest* request = (HttpRequest*)handle->data;
+    HttpServer *parent = (HttpServer *)request->parent_server;
+
+    map<int, HttpRequest *>::iterator iter = parent->requestMap_.find(request->clientid);
+    if (iter != parent->requestMap_.end()) {
+        freeHttpRequestCtx(iter->second);
+        parent->requestMap_.erase(iter);
+    }
+}
+
+void HttpServer::allocBufForRecvCallback(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf)
+{
+	HttpRequest * request = (HttpRequest*)handle->data;
+    *buf = request->recvBuf;
+}
+
+void HttpServer::onReadCallback(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf)
+{
+	HttpRequest* request = (HttpRequest*)stream->data;
+    if (request == NULL) {
+        AC_ERROR("Lost HttpRequest, can not distinguist tcpclient!");
+        return;
+    }
+    //assert(request);
+	HttpServer* parent = (HttpServer*)request->parent_server;
+    if (parent == NULL) {
+        AC_ERROR("Lost HttpServer handle");
+        return;
+    }
+    if (nread < 0) {
+        if (nread == UV_EOF) {
+            AC_INFO("client(%d) close(EOF)", request->clientid);
+           
+        } else if (nread == UV_ECONNRESET) {
+            AC_INFO("client(%d) close(conn reset)", request->clientid);
+           
+        } else {
+            AC_INFO("client(%d) close", request->clientid);      
+        }
+        uv_close((uv_handle_t*)&request->tcpHandle, onClientCloseCallback);   
+        return;
+    }
+    int parsed = http_parser_execute(&http_request->parser, &parserparserSettings__, buf.base, nread);
+  
+}
+
+void HttpServer::setReceiveCallback(userRecvCallback callback)
+{
+	recvcb_ = callback;
+}
+
+bool HttpServer::run()
+{
+	int ret = uv_run(&loop_, UV_RUN_DEFAULT);
+	if (ret) {
+		AC_ERROR("there are still active handles or requests");
+		return false;
+	}
+	return true;
+} 
+
+
+
+int HttpServer::getAvailableClientID() const
+{
+    static int s_id = 0;
+    return ++s_id;
+}
+
+string HttpServer::getUVError(int errcode)
+{
+    if(errcode == 0) {
+        return "";
+    }
+    string errMsg;
+    const char* tmpChar = uv_err_name(errcode);
+    if (tmpChar) {
+        errMsg = tmpChar;
+        errMsg += ":";
+    } else {
+        errMsg = "unknown system errcode";
+        errMsg += ":";
+    }
+    
+    tmpChar = uv_strerror(errcode);
+    if (tmpChar) {
+        errMsg += tmpChar;
+    }
+    tmpChar = NULL;
+    return errMsg;
+
+}
+
+void HttpServer::httpMessageBeginCallback(http_parser* parser)
+{
+    HttpRequest * request = parser->data;
+    request->headerLines = 0;
+}
+
+void HttpServer::httpUrlCallback(http_parser* parser, const char* chunk, size_t len)
+{
+    HttpRequest *request = parser->data;
+    request->url = chunk;
+}
+
+void HttpServer::httpHeaderFeildCallback(http_parser* parser, const char* chunk, size_t len)
+{
+    HttpRequest *request = parser->data;
+    HttpHeaderLine *headerLine = &request->headerLines[request->headerLines];
+    headerLine->feild =chunk;
+    headerLine->feild_length = len;
+}
+
+void HttpServer::httpHeaderValueCallback(http_parser* parser, const char* chunk, size_t len)
+{
+    HttpRequest *request = parser->data;
+    HttpHeaderLine *headerLine = request->headerLines[request->headerLines];
+    headerLine->value_length = len;
+    headerLine->value = chunk;
+    ++request->headerLines;
+}
+
+void HttpServer::httpHeadersCompleteCallback(http_parser* parser)
+{
+    HttpRequest *request = parser->data;
+    const char* method = http_method_str(parser->method);
+    request->method = malloc(sizeof(method));
+    strncpy(request->method, method, strlen(method));
+
+}
+
+void HttpServer::httpBodyCallback(http_parser* parser, const char* chunk, size_t len)
+{
+    HttpRequest *request = parser->data;
+    request->body = malloc(len+ 1);
+    request->body = chunk;
+}
+
+void HttpServer::httpMessageCompleteCallBack(http_parser* parser)
+{
+    HttpRequest *request = parser->data;
+    printf("url: %s\n", request->url);
+    printf("method: %s\n", request->method);
+    for (int i = 0; i < 5; i++) {
+        HttpHeader* header = &request->headers[i];
+        if (header->field)
+            printf("Header: %s: %s\n", header->field, header->value);
+    }
+    printf("body: %s\n", request->body);
+    printf("\r\n");
+
+    if (request->parent_server->recvcb_) {
+    	request->parent_server->recvcb_(request->clientid, request->url, request->method, request->body, sizeof(request->body)); 
+    }	
+}
---
+++ HttpServer.h
@@ -0,0 +1,172 @@
+﻿#include <tr1/functional>
+#include "uv.h"
+#include <string>
+#include <list>
+#include <map>
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE (1024*10)
+#endif
+
+#define MAX_HTTP_HEADERS 20
+
+//TCPClient接收到客户端数据回调给用户
+typedef   std::tr1::function<void (int clientid, const char* url, const char* method, const char* data, int size)>   userRecvCallback;
+
+
+/*****************************************************/
+//写请求结构体
+typedef struct {
+  uv_write_t req;  	//请求
+  uv_buf_t buf;		//保存发送的数据
+  int clientid;		//发送目标客户端
+} WriteReq_t;
+//创建一个写请求
+WriteReq_t * allocWriteParam(void);
+//销毁一个写请求
+void freeWriteParam(WriteReq_t* param);
+/*****************************************************/
+
+
+//单行http请求头部的信息
+typedef struct http_head_t
+{
+    string field;
+    string value;
+    size_t field_length;   
+    size_t value_length;
+}HttpHeaderLine;
+
+
+/*****************************************************/
+//保存client上下文的结构体
+typedef struct http_request_t {
+	uv_tcp_t tcpHandle;		//tcp handle
+    uv_buf_t recvBuf;		//接受消息的buf
+	int clientid;			//客户端标识id
+	void* parent_server;	//保存this指针
+    http_parser parser;     //每个请求独立的parser对象
+    string url;              
+    string method;
+    int headerLines;
+    HttpHeaderLine headerLines[MAX_HTTP_HEADERS];
+    string body;
+}HttpRequest;
+
+
+//创建一个request上下文	
+HttpRequest* allocHttpRequestCtx(void* parentserver);
+//释放一个request上下文
+void freeHttpRequestCtx(HttpRequest* ctx);
+/****************************************************/
+
+
+
+class HttpServer
+{
+public:
+	/*****************************************************
+     * @param maxPackageSize: 	数据包的最大值，读或写,目前不支持自定义，默认10k
+     * @param reconnectTimeout:	断开重连时间间隔
+     ****************************************************/
+	HttpServer(int maxClientNum,int maxPackageSize=BUFFER_SIZE); 
+
+	~HttpServer();
+	/*****************************************************
+     * @brief 设置接受消息事件回调函数
+     * @param callback: 	回调函数名称
+     ****************************************************/
+	void setReceiveCallback(userRecvCallback callback);
+
+	/*****************************************************
+     * @brief 是否启用 Nagle’s 算法
+     * @param enable: 		
+     *  true 	启用
+     *  false 	禁用
+     * @return 返回设置结果:
+     *  true: 	设置成功
+     *  false: 	失败
+     ****************************************************/
+    bool setNoDelay(bool enable);
+
+	/*****************************************************
+     * @brief 是否启用TCP keep-alive
+     * @param enable: 		
+     *  true 	启用
+     *  false 	禁用
+     * @param delay:	延时时间,以秒为单位,当enable为0时吗,值将被忽略
+     * @return 返回设置结果:
+     *  true: 	设置成功
+     *  false: 	失败
+     ****************************************************/
+    bool setKeepAlive(int enable, unsigned int delay);	
+
+    /*****************************************************
+     * @brief 连接服务器
+     * @param ip: 		服务器ip地址
+     * @param port:		连接端口
+     * @return 返回连接结果:
+     *  true: 	成功
+     *  false: 	失败
+     ****************************************************/
+	bool start(const char* ip, int port); 
+
+	/*****************************************************
+     * @brief 向编号为client_id的客户端发送数据
+     * @param data：	发送的数据
+     * @param len:		数据长度
+     * @return 返回连接结果:
+     *  -1: 	数据有误
+     *   0: 	数据无误
+     ****************************************************/
+	int  send(int client_id, char* data, int len);
+
+	/*****************************************************
+     * @brief	关闭client,程序退出时必须调用
+     ****************************************************/
+	void close();   
+
+    /*****************************************************
+     * @brief   等待loop线程退出
+     ****************************************************/
+    void join();   
+					
+protected:
+	int  getAvailableClientID() const;
+	bool init();
+	bool run();
+	int  sendToClient();
+	//成员函数做回调函数
+	static void onAcceptConnectionCallback(uv_stream_t* req, int status);
+	static void onReadCallback(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf);
+	static void onWriteCallback(uv_write_t* req, int status);
+	static void onClientCloseCallback(uv_handle_t* handle);
+	static void allocBufForRecvCallback(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf);
+	static void asyncCallback(uv_async_t* handle);
+	static void closeWalkCallback(uv_handle_t* handle, void* arg);
+	static void loopRunThread(void* arg);              
+	//解析http请求的回调函数
+    static void httpMessageBeginCallback(http_parser* parser);
+    static void httpUrlCallback(http_parser* parser, const char* chunk, size_t len);
+    static void httpHeaderFeildCallback(http_parser* parser, const* chunk, size_t len);
+    static void httpHeaderValueCallback(http_parser* parser, const* chunk, size_t len);
+    static void httpHeadersCompleteCallback(http_parser* parser);
+    static void httpBodyCallback(http_parser* parser, const char* chunk, size_t len);
+    static void httpMessageCompleteCallBack(http_parser* parser);
+	
+private:
+	std::string getUVError(int err);
+	std::string serverIp_;
+	int serverPort_;
+	uv_loop_t loop_; 
+	uv_tcp_t 	serverTcpHandle_;
+	uv_thread_t	runThreadHandle_;
+	uv_async_t	asyncHandle_;		
+    http_parser_settings parserSettings_;
+	uv_mutex_t mutexWrite_;					//mutex of writeReqList_
+	userRecvCallback recvcb_;				//接受数据回调
+	int maxPackageSize_;					//数据包最大值	
+    int maxClientNum_;
+	std::list<WriteReq_t*> writeReqList_;  	//后期用ringbuf	
+	std::map<int, TcpClientContext*> clientContextMap_;
+    //uv_mutex_t mutexContext_; 
+};
